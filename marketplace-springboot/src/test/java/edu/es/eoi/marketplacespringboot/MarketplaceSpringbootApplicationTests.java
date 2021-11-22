c203b2994d1e63f032868c34f332c73bc511bddd
jotajota1302 <jotajota1302@hotmail.com>
2021-11-22 15:26:37 +0100

    Add files via upload

---
+++ marketplace-springboot/src/test/java/edu/es/eoi/marketplacespringboot/MarketplaceSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.es.eoi.marketplacespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
