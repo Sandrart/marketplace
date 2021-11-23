8cd94faf91772717c9ae4c206d0470fd58d7def4
marcos gil <marcosgil1996@gmail.com>
2021-11-23 12:43:51 +0100

    commit

--- marketplace-springboot/src/main/java/edu/es/eoi/marketplacespringboot/repository/PedidosArticulosRepository.java
+++ marketplace-springboot/src/main/java/edu/es/eoi/marketplacespringboot/repository/PedidosArticulosRepository.java
@@ -1,7 +1,5 @@
 package edu.es.eoi.marketplacespringboot.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
